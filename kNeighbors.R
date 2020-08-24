8a70ffd03d9d650e96bea10a595ad1700797ddf9
Bjarne Schmalbach <55752891+bschmalbach@users.noreply.github.com>
2020-08-24 16:24:27 +0200

    Add files via upload

--- kNeighbors.R
+++ kNeighbors.R
@@ -6,6 +6,7 @@ df <- iris
 df <- dplyr::mutate(df, Sepal.Length=scale(Sepal.Length), Sepal.Width=scale(Sepal.Width), Petal.Length=scale(Petal.Length), Petal.Width=scale(Petal.Width))
 
 #randomly split data 70/30
+set.seed(5)
 split <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7, 0.3))
 df_train <- df[split,]
 df_test <- df[!split,]
@@ -20,10 +21,12 @@ table(df_test$true) # error rate of 3.6%
 rates<-0
 
 for (i in 1:50) {
+  set.seed(4)
   df_test$pred <- knn(df_train[1:4], df_test[1:4], df_train$Species, k=i)
   df_test$true <- F
   df_test$true[df_test$pred == df_test$Species] <- T
   rates[i] <- table(df_test$true)[1]/sum(table(df_test$true))
 }
 
-plot(rates, type = "l") # still low error rate (less than 5%) with k=4
+plot(rates, type = "l") # best error rates (around 6%) with k between 6 and 8
+
